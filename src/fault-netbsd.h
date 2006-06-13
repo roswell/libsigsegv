4bb14c6c705cd7d6ad2a2168e6f180be5394d63d
Bruno Haible <bruno@clisp.org>
2006-06-13 17:52:13 +0000

    Generalize NetBSD/i386 support to other architectures.

--- src/fault-netbsd.h
+++ src/fault-netbsd.h
@@ -16,3 +16,14 @@
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
 
 #include "fault-posix.h"
+
+/* _UC_MACHINE_SP is a platform independent macro.
+   Defined in <machine/mcontext.h>, see
+     http://cvsweb.netbsd.org/bsdweb.cgi/src/sys/arch/$arch/include/mcontext.h
+   Supported on alpha, amd64, i386, ia64, m68k, mips, powerpc, sparc since
+   NetBSD 2.0.
+   On i386, _UC_MACHINE_SP is the same as ->uc_mcontext.__gregs[_REG_UESP],
+   and apparently the same value as       ->uc_mcontext.__gregs[_REG_ESP]. */
+#ifdef _UC_MACHINE_SP
+#define SIGSEGV_FAULT_STACKPOINTER  _UC_MACHINE_SP ((ucontext_t *) ucp)
+#endif
