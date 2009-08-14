b7646d073335db66e08107434482e095ff2962da
Bruno Haible <bruno@clisp.org>
2009-08-14 17:18:19 +0200

    Remove config.guess, config.sub from from version control.

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,10 @@
 # with new versions of autoconf or automake.
 #
 # This script requires autoconf-2.63 and automake-1.11 in the PATH.
+# It also requires either
+#   - the GNULIB_TOOL environment variable pointing to the gnulib-tool script
+#     in a gnulib checkout, or
+#   - an internet connection.
 
 # Copyright (C) 2009 Free Software Foundation, Inc.
 #
@@ -22,6 +26,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Usage: ./autogen.sh
+
+# Fetch config.guess, config.sub.
+if test -n "$GNULIB_TOOL"; then
+  for file in config.guess config.sub; do
+    $GNULIB_TOOL --copy-file build-aux/$file; chmod a+x build-aux/$file
+  done
+else
+  for file in config.guess config.sub; do
+    wget -q --timeout=5 -O build-aux/$file.tmp "http://git.savannah.gnu.org/gitweb/?p=gnulib.git;a=blob_plain;f=build-aux/${file};hb=HEAD" \
+      && mv build-aux/$file.tmp build-aux/$file \
+      && chmod a+x build-aux/$file
+  done
+fi
 
 # Generate aclocal.m4.
 aclocal -I m4
