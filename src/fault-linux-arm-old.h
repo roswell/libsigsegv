784abfdb25b8af78d78600c145b2cc16580d28e0
Bruno Haible <bruno@clisp.org>
2021-05-16 20:35:25 +0200

    Modernize copyright notices: Replace FSF snail-mail addresses with URLs.

--- src/fault-linux-arm-old.h
+++ src/fault-linux-arm-old.h
@@ -1,10 +1,10 @@
 /* Fault handler information.  Linux/ARM version.
    Copyright (C) 2002  Bruno Haible <bruno@clisp.org>
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* This file supports only kernels >= 2.2.14 or >= 2.3.35.  Support for older
    kernels would be more complicated, see file
--- tests/test-catch-segv1.c
+++ tests/test-catch-segv1.c
@@ -1,10 +1,10 @@
 /* Test that the handler is called, with the right fault address.
    Copyright (C) 2002-2006, 2008, 2011, 2016, 2021  Bruno Haible <bruno@clisp.org>
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #ifndef _MSC_VER
 # include <config.h>
