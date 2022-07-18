1fdec006215cea48352bcd3d08b3f058a2eb4d1f
Bruno Haible <bruno@clisp.org>
2022-07-18 18:09:33 +0200

    Move gnulib-tool invocation back from autopull.sh to autogen.sh.
    * autogen.sh: Revert last change. Improve comments.
    * autopull.sh: Only call 'gitsub.sh pull'.

--- autogen.sh
+++ autogen.sh
@@ -21,8 +21,56 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Prerequisite (if not used from a released tarball): ./autopull.sh
-# Usage: ./autogen.sh
+# Prerequisite (if not used from a released tarball): either
+#   - the GNULIB_SRCDIR environment variable pointing to a gnulib checkout, or
+#   - a preceding invocation of './autopull.sh'.
+#
+# Usage: ./autogen.sh [--skip-gnulib]
+#
+# Options:
+#   --skip-gnulib       Avoid fetching files from Gnulib.
+#                       This option is useful
+#                       - when you are working from a released tarball (possibly
+#                         with modifications), or
+#                       - as a speedup, if the set of gnulib modules did not
+#                         change since the last time you ran this script.
+
+skip_gnulib=false
+while :; do
+  case "$1" in
+    --skip-gnulib) skip_gnulib=true; shift;;
+    *) break ;;
+  esac
+done
+
+if test $skip_gnulib = false; then
+  if test -n "$GNULIB_SRCDIR"; then
+    test -d "$GNULIB_SRCDIR" || {
+      echo "*** GNULIB_SRCDIR is set but does not point to an existing directory." 1>&2
+      exit 1
+    }
+  else
+    GNULIB_SRCDIR=`pwd`/gnulib
+    test -d "$GNULIB_SRCDIR" || {
+      echo "*** Subdirectory 'gnulib' does not yet exist. Use './gitsub.sh pull' to create it, or set the environment variable GNULIB_SRCDIR." 1>&2
+      exit 1
+    }
+  fi
+  # Now it should contain a gnulib-tool.
+  GNULIB_TOOL="$GNULIB_SRCDIR/gnulib-tool"
+  test -f "$GNULIB_TOOL" || {
+    echo "*** gnulib-tool not found." 1>&2
+    exit 1
+  }
+  $GNULIB_TOOL --copy-file m4/musl.m4
+  $GNULIB_TOOL --copy-file m4/relocatable-lib.m4
+  $GNULIB_TOOL --copy-file m4/sigaltstack.m4
+  $GNULIB_TOOL --copy-file m4/stack-direction.m4
+  # Fetch config.guess, config.sub.
+  for file in config.guess config.sub; do
+    $GNULIB_TOOL --copy-file build-aux/$file; chmod a+x build-aux/$file
+  done
+fi
 
 # Generate aclocal.m4.
 aclocal -I m4
--- autopull.sh
+++ autopull.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 # Convenience script for fetching auxiliary files that are omitted from
 # the version control repository of this package.
-#
-# This script requires either
-#   - the GNULIB_SRCDIR environment variable pointing to a gnulib checkout, or
-#   - a preceding invocation of './gitsub.sh pull'.
 
-# Copyright (C) 2009-2022 Free Software Foundation, Inc.
+# Copyright (C) 2022 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,33 +19,6 @@
 
 # Usage: ./autopull.sh
 
-./gitsub.sh pull
-
-if test -n "$GNULIB_SRCDIR"; then
-  test -d "$GNULIB_SRCDIR" || {
-    echo "*** GNULIB_SRCDIR is set but does not point to an existing directory." 1>&2
-    exit 1
-  }
-else
-  GNULIB_SRCDIR=`pwd`/gnulib
-  test -d "$GNULIB_SRCDIR" || {
-    echo "*** Subdirectory 'gnulib' does not yet exist. Use './gitsub.sh pull' to create it, or set the environment variable GNULIB_SRCDIR." 1>&2
-    exit 1
-  }
-fi
-# Now it should contain a gnulib-tool.
-GNULIB_TOOL="$GNULIB_SRCDIR/gnulib-tool"
-test -f "$GNULIB_TOOL" || {
-  echo "*** gnulib-tool not found." 1>&2
-  exit 1
-}
-$GNULIB_TOOL --copy-file m4/musl.m4
-$GNULIB_TOOL --copy-file m4/relocatable-lib.m4
-$GNULIB_TOOL --copy-file m4/sigaltstack.m4
-$GNULIB_TOOL --copy-file m4/stack-direction.m4
-# Fetch config.guess, config.sub.
-for file in config.guess config.sub; do
-  $GNULIB_TOOL --copy-file build-aux/$file; chmod a+x build-aux/$file
-done
+./gitsub.sh pull || exit 1
 
 echo "$0: done.  Now you can run './autogen.sh'."
