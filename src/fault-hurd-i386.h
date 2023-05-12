ef3f3e980cf4f2c7d6ab723436bcd68ecefbf177
Bruno Haible <bruno@clisp.org>
2023-05-12 21:23:03 +0200

    Tentative support for Hurd/x86_64.
    Reported by Samuel Thibault <samuel.thibault@ens-lyon.org>.
    
    * src/fault-hurd-i386.h: Add support for x86_64 CPU.
    * configure.ac (CFG_FAULT): Use fault-hurd-i386.h also on x86_64.

--- src/fault-hurd-i386.h
+++ src/fault-hurd-i386.h
@@ -1,5 +1,5 @@
-/* Fault handler information.  Hurd/i386 version.
-   Copyright (C) 2017  Bruno Haible <bruno@clisp.org>
+/* Fault handler information.  Hurd/i386 and Hurd/x86_64 versions.
+   Copyright (C) 2017, 2023  Bruno Haible <bruno@clisp.org>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,11 +16,29 @@
 
 #include "fault-hurd.h"
 
+#if defined __x86_64__
+/* 64 bit registers */
+
+/* scp points to a 'struct sigcontext' (defined in
+   glibc/sysdeps/mach/hurd/x86_64/bits/sigcontext.h).
+   The registers, at the moment the signal occurred, get pushed on the stack
+   through gnumach/x86_64/locore.S:alltraps and then copied into the struct
+   through glibc/sysdeps/mach/hurd/x86/trampoline.c.  */
+/* sc_rsp is unused (not set by gnumach/x86_64/locore.S:alltraps).  We need
+   to use sc_ursp.  */
+# define SIGSEGV_FAULT_STACKPOINTER  scp->sc_ursp
+
+#else
+/* 32 bit registers */
+
 /* scp points to a 'struct sigcontext' (defined in
    glibc/sysdeps/mach/hurd/i386/bits/sigcontext.h).
-   The registers of this struct get pushed on the stack through
-   gnumach/i386/i386/locore.S:trapall.  */
+   The registers, at the moment the signal occurred, get pushed on the stack
+   through gnumach/i386/i386/locore.S:alltraps and then copied into the struct
+   through glibc/sysdeps/mach/hurd/x86/trampoline.c.  */
 /* Both sc_esp and sc_uesp appear to have the same value.
    It appears more reliable to use sc_uesp because it is labelled as
    "old esp, if trapped from user".  */
-#define SIGSEGV_FAULT_STACKPOINTER  scp->sc_uesp
+# define SIGSEGV_FAULT_STACKPOINTER  scp->sc_uesp
+
+#endif
